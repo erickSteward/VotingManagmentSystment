39ca37fe3b2f2650a98db2f7ff64e1b03a6bb85d
eruicSteward <erickmwangimaina8@gmail.com>
2025-04-14 19:44:02 +0300

    Null Objects Issues

--- src/controller/CitizenController.java
+++ src/controller/CitizenController.java
@@ -1,32 +1,31 @@
 package controller;
 
-// import java.util.Date;
-import java.util.Scanner;
-
 import sevice.CitizenService;
 import sevice.MenuService;
 
+import java.util.Scanner;
+
 public class CitizenController {
-  private Scanner scanner = new Scanner(System.in);
-  private CitizenService citizenServices = new CitizenService();
-  private MenuService menuService = new MenuService();
+ private Scanner scanner = new Scanner(System.in);
+ private CitizenService citizenServices;
+ private MenuService menuService;
 
   public void run(){
     menuService.mainMenu();
   }
 
-  public boolean registerCitizen(){
-    if(menuService.registerCitizenMenu()){
-      System.out.println("Citizen Register Successfully! ");
-      return true;
-    }
-    System.out.println("Failed to register.");
-    menuService.doContinue();
-    return false;
-  }
+   public boolean registerCitizen(){
+     if(menuService.registerCitizenMenu()){
+       System.out.println("Citizen Register Successfully! ");
+       return true;
+     }
+     System.out.println("Failed to register.");
+     menuService.doContinue();
+     return false;
+   }
 
-  public void retrieveAllCitizens(){
-    citizenServices.getAllCitizens();
-  }
+   public void retrieveAllCitizens(){
+     citizenServices.getAllCitizens();
+   }
   
 }
--- src/sevice/MenuService.java
+++ src/sevice/MenuService.java
@@ -7,11 +7,10 @@ import controller.CitizenController;
 public class MenuService {
 
   private Scanner scanner = new Scanner(System.in);
-  CitizenService citizenServices = new CitizenService();
-  CitizenController citizenController = new CitizenController();
+  CitizenService citizenServices;
+  CitizenController citizenController;
 
   public void mainMenu() {
-    try (Scanner scanner = new Scanner(System.in)) {
       System.out.println("********* Welcome to the Voting Management System ********* ");
       System.out.println("Please select the actions you need. ");
       System.out.println("1. Register Citizen. ");
@@ -20,71 +19,70 @@ public class MenuService {
       int choice = scanner.nextInt();
       switch (choice) {
         case 1:
-          citizenController.registerCitizen();
+          //citizenController.registerCitizen();
           break;
         case 2:
-          citizenController.retrieveAllCitizens();
+          //citizenController.retrieveAllCitizens();
           break;
         default:
           System.out.println("bye");
           break;
       }
     }
-  }
 
-  public boolean registerCitizenMenu() {
-      System.out.println("Please register the following citizen details as prompted : ");
+   public boolean registerCitizenMenu() {
+       System.out.println("Please register the following citizen details as prompted : ");
 
-      System.out.print("Birth Certificate Number : ");
-      int birthCertificateNo = scanner.nextInt();
+       System.out.print("Birth Certificate Number : ");
+       int birthCertificateNo = scanner.nextInt();
 
-      System.out.print("First Name : ");
-      String fistName = scanner.next();
+       System.out.print("First Name : ");
+       String fistName = scanner.next();
 
-      System.out.print("Last Name : ");
-      String lastName = scanner.next();
+       System.out.print("Last Name : ");
+       String lastName = scanner.next();
 
-      System.out.print("National ID : ");
-      int nationalId = scanner.nextInt();
+       System.out.print("National ID : ");
+       int nationalId = scanner.nextInt();
 
-      System.out.print("Location : ");
-      String location = scanner.next();
+       System.out.print("Location : ");
+       String location = scanner.next();
 
-      System.out.print("Ward : ");
-      String ward = scanner.next();
+       System.out.print("Ward : ");
+       String ward = scanner.next();
 
-      System.out.print("Constituency : ");
-      String constituency = scanner.next();
+       System.out.print("Constituency : ");
+       String constituency = scanner.next();
 
-      System.out.print("County : ");
-      String county = scanner.next();
+       System.out.print("County : ");
+       String county = scanner.next();
 
-      System.out.print("Ethnicity : ");
-      String ethnicity = scanner.next();
+       System.out.print("Ethnicity : ");
+       String ethnicity = scanner.next();
 
-      if (citizenServices.createCitizen(birthCertificateNo, fistName, lastName, nationalId, location, ward,
-          constituency, county, ethnicity)) {
-        doContinue();
-      }
-      System.out.println("Please key in all the details of the Citizen Properly. ");
-      return false;
-    }
+       boolean isCitizenAdded = citizenServices.createCitizen(birthCertificateNo, fistName, lastName, nationalId, location, ward,constituency, county, ethnicity);
+
+       if (!isCitizenAdded ) {
+         doContinue();
+       }
+       return isCitizenAdded;
+     }
 
 
-  public boolean doContinue() {
-    System.out.println("Do you want to continue? Yes? or No?");
-    String action = scanner.next().toLowerCase();
+   public boolean doContinue() {
+     System.out.println("Do you want to continue? Yes? or No?");
+     String action = scanner.next().toLowerCase();
 
-    switch (action) {
-      case "yes" -> registerCitizenMenu();
-      case "no"-> mainMenu();
-      default -> {
-        System.out.println("Please type 'Yes' to continue and 'No' to go back to the main menu");
-        doContinue();
-        break;
-      }
+     switch (action) {
+       case "yes" -> registerCitizenMenu();
+       case "no"-> mainMenu();
+       default -> {
+         System.out.println("Please type 'Yes' to continue and 'No' to go back to the main menu");
+         doContinue();
+         break;
+       }
       
-    }
-    return false;
-  }
+     }
+     return false;
+   }
 }
